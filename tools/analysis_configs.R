d51e20fee6fca666dbc9d5a9427052ec0da23f69
gveran22 <127981206+gveran22@users.noreply.github.com>
2026-06-22 21:08:24 +0200

    update of taxonomy parameters for proportional filtering

--- tools/analysis_configs.R
+++ tools/analysis_configs.R
@@ -1,5 +1,5 @@
 analysis_configs <- list(
-  list(agg_level = "Order", filtTax = "numbSamp", filtTaxPar = list(numbSamp = 5), filtSamp = "totalReads", filtSampPar = list(totalReads = 500)),
-  list(agg_level = "Family", filtTax = "numbSamp", filtTaxPar = list(numbSamp = 5), filtSamp = "totalReads", filtSampPar = list(totalReads = 500)),
-  list(agg_level = "Genus", filtTax = "highestVar", filtTaxPar = list(highestVar = 100), filtSamp = "totalReads", filtSampPar = list(totalReads = 500))
+  list(agg_level = "Order", filtTax = "numbSamp", filtTaxPar = list(numbSamp_prop = 0.01), filtSamp = "totalReads", filtSampPar = list(totalReads_quant = 0.02)),
+  list(agg_level = "Family", filtTax = "numbSamp", filtTaxPar = list(numbSamp_prop = 0.01), filtSamp = "totalReads", filtSampPar = list(totalReads_quant = 0.02)),
+  list(agg_level = "Genus", filtTax = "highestVar", filtTaxPar = list(numbSamp_prop = 0.01), filtSamp = "totalReads", filtSampPar = list(totalReads_quant = 0.02))
 )
