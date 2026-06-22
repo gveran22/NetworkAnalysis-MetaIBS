1d0fdf2f4ee3496701838172a74b508787489a84
gveran22 <127981206+gveran22@users.noreply.github.com>
2026-06-22 21:07:33 +0200

    adding taxonomy level parameters for network comparison

---
+++ tools/analysis_configs_comparison.R
@@ -0,0 +1,5 @@
+analysis_configs <- list(
+  list(agg_level = "Order", filtTax = "numbSamp", filtTaxPar = list(numbSamp_prop = 0.01), filtSamp = "totalReads", filtSampPar = list(totalReads_quant = 0.03))
+  #list(agg_level = "Family", filtTax = "numbSamp", filtTaxPar = list(numbSamp_prop = 0.01), filtSamp = "totalReads", filtSampPar = list(totalReads_quant = 0.03)),
+  #list(agg_level = "Genus", filtTax = "highestVar", filtTaxPar = list(numbSamp_prop = 0.01), filtSamp = "totalReads", filtSampPar = list(totalReads_quant = 0.03))
+)
